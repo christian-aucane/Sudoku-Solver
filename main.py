26aaf6f55071efd8ac143b4b19e3bf875b783c83
Christian Aucane <christianaucane1@gmail.com>
2024-03-18 14:16:14 +0100

    Refactor

--- main.py
+++ main.py
@@ -8,14 +8,20 @@ def read_file(file_name):
             grid.append(line)
     return grid
 
-
-def main():
+def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('file_name')
     parser.add_argument('method', choices=["bruteforce", "backtrack"], default="bruteforce")
     parser.add_argument('interface', choices=["cli", "gui"], default="cli")
     args = parser.parse_args()
-    grid = read_file(args.file_name + '.txt')
+
+    return args
+
+def main():
+    
+    args = parse_args()
+
+    grid = read_file("grids/" + args.file_name + '.txt')
 
     if args.method == 'bruteforce':
         from bruteforce import BruteForceSudokuSolver
