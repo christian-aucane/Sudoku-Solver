1580bb435b569d9a0514897293518fa1a1f02a47
Christian Aucane <christianaucane1@gmail.com>
2024-03-19 14:52:27 +0100

    Add docstrings and help texts

--- src/main.py
+++ src/main.py
@@ -1,3 +1,17 @@
+"""
+Main module
+
+This module contains the main function
+
+Arguments
+---------
+
+file_name : str
+    name of the file containing the grid withpout the extension
+method : str
+    method to solve the sudoku (bruteforce, backtracking, mook)
+interface : str
+    interface to use (cli, gui)"""
 import argparse
 from pathlib import Path
 
@@ -29,25 +43,27 @@ def parse_args():
         command line arguments
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('file_name')
-    parser.add_argument('method', choices=["bruteforce", "backtrack", "mook"], default="bruteforce")
-    parser.add_argument('interface', choices=["cli", "gui"], default="cli")
+    parser.add_argument('file_name', type=str, help='name of the file containing the grid withpout the extension')
+    parser.add_argument('method', choices=["bruteforce", "backtrack", "mook"], default="bruteforce", help="method to solve the sudoku (bruteforce, backtracking, mook)")
+    parser.add_argument('interface', choices=["cli", "gui"], default="cli", help="interface to use (cli, gui)")
     args = parser.parse_args()
 
     return args
 
 def main():
-    
+    """
+    Main function
+    """
     args = parse_args()
 
     grid = read_file(GRIDS_PATH / (args.file_name + '.txt'))
 
-    if args.method == 'bruteforce':
-        from bruteforce import BruteForceSudokuSolver
-        solver = BruteForceSudokuSolver(grid)
-    elif args.method == 'backtrack':
-        from backtrack import BacktrackSudokuSolver
-        solver = BacktrackSudokuSolver(grid)
+    if args.method == 'backtracking':
+        from solvers.backtracking import BacktrackingSudokuSolver
+        solver = BacktrackingSudokuSolver(grid)
+    elif args.method == 'bruteforce':
+        from solvers.bruteforce import BruteforceSudokuSolver
+        solver = BruteforceSudokuSolver(grid)
     elif args.method == 'mook':
         from solvers.mook import MookSudokuSolver
         solver = MookSudokuSolver(grid)
