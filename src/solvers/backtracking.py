9b8b6135432801664e7bbf31afcdb786e1c97b7b
Christian Aucane <christianaucane1@gmail.com>
2024-03-22 12:49:39 +0100

    Add print for number of empty cells and number of possibles combinations

--- src/solvers/backtracking.py
+++ src/solvers/backtracking.py
@@ -4,6 +4,13 @@ from solvers.base import BaseSudokuSolver
 class BacktrackingSudokuSolver(BaseSudokuSolver):
 
     def solve(self):
+        
+        print(f"Number of empty cells: {self.n_empty_cells}")
+        print(f"Number of possible combinations: {self.n_combinations:.2e}")
+
+        self.solve_recursive()
+
+    def solve_recursive(self):
         row, col = self.find_empty_cell()
         if row is None and col is None:
             return True
@@ -11,7 +18,7 @@ class BacktrackingSudokuSolver(BaseSudokuSolver):
             if self.is_valid(row, col, num):
                 self.grid[row][col] = num
                 self.display()
-                if self.solve():
+                if self.solve_recursive():
                     return True
                 self.grid[row][col] = 0
                 self.display()
